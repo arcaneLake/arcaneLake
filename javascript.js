9bef4954d4a4298b255efb708f97ef8b724c37cc
arcaneLake <geisnerm@gmail.com>
2022-06-28 22:39:34 +0900

    Add a few JS exercises

--- javascript.js
+++ javascript.js
@@ -1,4 +1,24 @@
 "use strict";
 
-document.write("Hi.");
+function favoriteAnimal(animal) {
+    document.write(animal + " is my favorite animal!");
+}
 
+favoriteAnimal("Goat");
+
+function add7(num) {
+    console.log(num + 7);
+}
+add7(10);
+
+function capitalize(string1) {
+    let string2 = string1.charAt(0).toUpperCase() + string1.slice(1).toLowerCase();
+    console.log(string2);
+}
+capitalize("tesTing");
+
+function lastLetter(sometext)  {
+    let modifiedtext = sometext.slice(-1);
+    console.log(modifiedtext);
+}
+lastLetter("Geisner");
